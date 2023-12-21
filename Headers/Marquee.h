0e5a1e42371cf6de6b44ffe66c3aaa3000e77969
Coder45284 <126514127+Coder45284@users.noreply.github.com>
2023-12-21 13:35:51 -0800

    Hopefully it is now properly converted
    This is the new command.
    #!/bin/bash
    
    for filename in Headers/*.h; do
        echo Converting "$filename"
        iconv -f macintosh -t UTF-8 "$filename" | tr '\r' '\n' | \
    	tr -s '\n' | expand --tabs=4 > "$filename.new"
        rm "$filename"
        mv "$filename.new" "$filename"
    done
    
    for filename in Sources/*.c; do
        echo Converting "$filename"
        iconv -f macintosh -t UTF-8 "$filename" | tr '\r' '\n' | \
    	tr -s '\n' | expand --tabs=4 > "$filename.new"
        rm "$filename"
        mv "$filename.new" "$filename"
    done

--- Headers/Marquee.h
+++ Headers/Marquee.h
@@ -1,1 +1,15 @@
-//============================================================================//----------------------------------------------------------------------------//									Marquee.h//----------------------------------------------------------------------------//============================================================================#pragma once#include <Quickdraw.h>typedef struct{	Pattern		pats[kNumMarqueePats];	Rect		bounds, handle;	short		index, direction, dist;	Boolean		active, paused, handled;} marquee;extern marquee		theMarquee;
+//============================================================================
+//----------------------------------------------------------------------------
+//                                  Marquee.h
+//----------------------------------------------------------------------------
+//============================================================================
+#pragma once
+#include <Quickdraw.h>
+typedef struct
+{
+    Pattern     pats[kNumMarqueePats];
+    Rect        bounds, handle;
+    short       index, direction, dist;
+    Boolean     active, paused, handled;
+} marquee;
+extern marquee      theMarquee;
